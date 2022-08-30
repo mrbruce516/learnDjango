56dc6fff160882373a2c111511838994df1d46eb
mrbruce516 <46187947+mrbruce516@users.noreply.github.com>
2022-08-30 17:39:55 +0800

    优化视图可读性，此类可以为表单添加BootStrap样式，方便复用

---
+++ web/utils/bsmodelform.py
@@ -0,0 +1,30 @@
+"""
+自定义ModelForm类，并添加BootStrap样式
+
+使用此类需要如下操作
+class xxx(BootStrapModelForm):
+    # 获取元数据
+    class Meta:
+        model = models.PrettyNum
+        # fields = ["mobile", "price", "level", "status"]
+        # __all__表示所有字段, exclude表示排除某个字段
+        fields = "__all__"
+"""
+from django import forms
+
+
+class BootStrapModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 循环ModelForm中的所有字段，给每个字段设置插件
+        for name, field, in self.fields.items():
+            # 字段中有属性的话，保留原来的属性，再添加
+            if field.widget.attrs:
+                field.widget.attrs["class"] = "form-control"
+                field.widget.attrs["placeholder"] = field.label
+            # 如果字段中没有属性的话，直接创建字典写入BS属性
+            else:
+                field.widget.attrs = {
+                    "class": "form-control",
+                    "placeholder": field.label
+                }
