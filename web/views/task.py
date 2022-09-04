a5db3f09b2056ba3625d6862e381d15dec11854d
mrbruce516 <46187947+mrbruce516@users.noreply.github.com>
2022-09-04 23:13:43 +0800

    完成任务页面，可以查看现有任务，并提交任务后通过js刷新页面

--- web/views/task.py
+++ web/views/task.py
@@ -2,14 +2,19 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from web import models
+from web.utils.pagination import Pagination
 from web.utils.form import TaskModelForm
 
 
 def task_list(request):
+    queryset = models.Task.objects.all()
+    page_obj = Pagination(request, queryset)
     form = TaskModelForm()
     context = {
-        'form': form
+        'form': form,
+        'queryset': queryset,
+        'page_number': page_obj.html()
     }
     return render(request, 'task_list.html', context)
 
